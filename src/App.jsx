2aaff0dabf70681499c411b8844e10b25be67e29
Narahari1994 <narahariv94@gmail.com>
2024-03-19 17:32:56 +0530

    todos final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-green-500">redux toolkit</h1>
+      <h1>Manage Your Todos</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
